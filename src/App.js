32bc42d18023677cc078db0090bdd29338254e40
Matija <matijaselendic@gmail.com>
2019-08-19 11:43:16 +0200

    Initial commit with initial components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import About from "./components/About";
+import Error from "./components/Error";
+import Boards from "./components/Boards";
+import Teams from "./components/Teams";
+
+import Topbar from "./shared/Topbar";
+import Sidebar from "./shared/Sidebar";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex" id="wrapper">
+        <Sidebar />
+        <div id="page-content-wrapper">
+
+          <Topbar />
+          
+          <div className="container-fluid">
+            <Switch>
+              <Route path="/" component={Home} exact/>
+              <Route path="/boards" component={Boards}/>
+              <Route path="/teams" component={Teams}/>
+              <Route path="/profile" component={Profile} />
+              <Route path="/about" component={About} />
+              <Route component={Error} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Error.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Error = () => {
+    return (
+        <div>Error: Path is wrong!</div>
+    );
+}
+
+export default Error;
